74ed417b2eb3cb447c55c881f8a18016ca120bec
toliar <Toliar@mail.ru>
2018-07-16 11:24:50 +0300

    Admin login

--- SeleniumFull/UnitTest1.cs
+++ SeleniumFull/UnitTest1.cs
@@ -33,7 +33,37 @@ namespace csharp_example
         {
             driver.Quit();
             driver = null;
-
         }
+    }
+
+    [TestFixture]
+    public class LoginTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://localhost:8889/litecart/admin";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+
     }
 }
